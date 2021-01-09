6561edc4a8fdf8702aaeb217790ed8a7d11ffaca
arminv <arminv@users.noreply.github.com>
2021-01-09 02:07:33 -0500

    Progress (mark: 1:01:54)

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
 import React from 'react';
 // NOTE: `SafeAreaView` allows for styling that is compatible with different devices:
-import {View, Text, SafeAreaView} from 'react-native';
+// NOTE: in React Native, all styling is done via `StyleSheet`:
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.safeArea}>
+      {/* NOTE: we can apply more than one style to an element by passing an array to the style prop: */}
+      {/* NOTE: multiple styles cascade, meaning the last style overwrites the previous ones (if there is overlap/common styling) */}
+      {/* NOTE: there is an equivalent for styled-components in React Native */}
+      <View style={[styles.container, styles.yellow]}>
         <Text>Hello world!</Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  yellow: {
+    backgroundColor: 'yellow',
+  },
+  container: {
+    // NOTE: we can use `vertical` and `horizontal`, which are equivalents of `top` & `bottom`, `left` & `right` in web development:
+    // NOTE: in React Native, there are no units (pixels, etc.):
+    // NOTE: another difference with web development is that in React Native we use camel case for styling (no dashes):
+    // NOTE: all positioning in React Native is done via flexbox (i.e. `display: flex`)
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    // paddingVertical: 10,
+    // paddingHorizontal: 30,
+    // borderColor: 'red',
+    // borderWidth: 2,
+  },
+  safeArea: {
+    flex: 2,
+  },
+});
 
 export default App;
