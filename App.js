44094b83fc424755ed2a05dd24620ad437c2b846
arminv <arminv@users.noreply.github.com>
2021-01-20 03:07:14 -0500

    Progress (mark 1:50:51)

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 // NOTE: `SafeAreaView` allows for styling that is compatible with different devices:
 // NOTE: in React Native, all styling is done via `StyleSheet`:
 import {View, Text, SafeAreaView, StyleSheet, FlatList} from 'react-native';
 import ColorBox from './components/ColorBox';
+import {NavigationContainer} from '@react-navigation/native';
 
 // NOTE: These colors are part of the Solarized color scheme by Ethan Schoonover:
 const COLORS = [
@@ -26,34 +28,36 @@ const COLORS = [
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      {/* NOTE: While it may be fine to do on the web, in React Native you should avoid using map in the render.
+    <NavigationContainer>
+      <SafeAreaView style={styles.safeArea}>
+        {/* NOTE: While it may be fine to do on the web, in React Native you should avoid using map in the render.
       This is because mapping over an array is not optimized.
       React Native will attempt to render every single element in the array all at once, regardless of whether they are visible on the screen or not.
       There are special components in React Native for rendering lists: these are FlatList and SectionList. */}
-      <FlatList
-        style={styles.container}
-        data={COLORS}
-        // NOTE: if our data has unique keys, we will NOT need to specify the `keyExtractor` prop here anymore!
-        keyExtractor={(item) => item.colorName}
-        // NOTE: we destructure `{item}` below because `renderItem` also returns the item id:
-        renderItem={({item}) => (
-          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-        )}
-        // NOTE: for `ListHeaderComponent` we can either pass a function that returns a component, or we can just pass the component itself:
-        ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
-      />
-      {/* NOTE: we can apply more than one style to an element by passing an array to the style prop: */}
-      {/* NOTE: multiple styles cascade, meaning the last style overwrites the previous ones (if there is overlap/common styling) */}
-      {/* NOTE: there is an equivalent for styled-components in React Native */}
-      {/* <View style={[styles.container, styles.yellow]}>
+        <FlatList
+          style={styles.container}
+          data={COLORS}
+          // NOTE: if our data has unique keys, we will NOT need to specify the `keyExtractor` prop here anymore!
+          keyExtractor={(item) => item.colorName}
+          // NOTE: we destructure `{item}` below because `renderItem` also returns the item id:
+          renderItem={({item}) => (
+            <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+          )}
+          // NOTE: for `ListHeaderComponent` we can either pass a function that returns a component, or we can just pass the component itself:
+          ListHeaderComponent={<Text style={styles.text}>Solarized</Text>}
+        />
+        {/* NOTE: we can apply more than one style to an element by passing an array to the style prop: */}
+        {/* NOTE: multiple styles cascade, meaning the last style overwrites the previous ones (if there is overlap/common styling) */}
+        {/* NOTE: there is an equivalent for styled-components in React Native */}
+        {/* <View style={[styles.container, styles.yellow]}>
         <Text style={styles.text}>Here are some boxes of different colors</Text>
         <ColorBox colorName="cyan" hexCode="#2aa198" />
         <ColorBox colorName="blue" hexCode="#268bd2" />
         <ColorBox colorName="magenta" hexCode="#d33682" />
         <ColorBox colorName="orange" hexCode="#cb4b16" />
       </View> */}
-    </SafeAreaView>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
